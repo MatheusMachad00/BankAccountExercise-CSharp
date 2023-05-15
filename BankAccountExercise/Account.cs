1315ebc9e5b5b9c199621cb524db946dd9653211
mathe <mathe@DESKTOP-F29R11R>
2023-05-15 16:07:14 -0300

    Adicionar arquivos de projeto.

---
+++ BankAccountExercise/Account.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace BankAccountExercise {
+    internal class Account {
+
+        private int AccountNumber { get; set; }
+        private string Name { get; set; }
+        private double Balance { get; set; }
+
+        public Account(int accountNumber, string name) {
+            AccountNumber = accountNumber;
+            Name = name;
+        }
+
+        public Account(int accountNumber, string name, double balance) {
+            AccountNumber = accountNumber;
+            Name = name;
+            Balance = balance;
+        }
+
+        public void Deposit(double value) {
+            Balance += value;
+        }
+
+        public void Withdraw(double value) {
+            Balance = Balance - (value + 5);
+        }
+        public string AccountData() {
+            return $"Conta: {AccountNumber}, Titular: {Name}, Saldo: R${Balance.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+    }
+}
---
+++ BankAccountExercise/Program.cs
@@ -0,0 +1,48 @@
+﻿using BankAccountExercise;
+using System;
+using System.Globalization;
+
+
+namespace MyApp {
+    internal class Program {
+
+        static void Main(string[] args) {
+
+            Account account;
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Bem vindo ao Banco de Axel!\n", Console.ForegroundColor);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("Digite o número da conta: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o nome do usuário: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Haverá um depósito inicial? (s/n) ");
+            char initialDeposit = char.Parse(Console.ReadLine());
+            if (initialDeposit == 's' || initialDeposit == 'S') {
+                Console.Write("Digite o valor do depósito: ");
+                double initialDepositValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account = new Account(number, username, initialDepositValue);
+            }
+            else {
+                account = new Account(number, username);
+            }
+
+            Console.WriteLine("\nDados da conta:");
+            Console.WriteLine(account.AccountData());
+
+            Console.Write("\nDigite um valor para depósito: ");
+            account.Deposit(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Conta com dados atualizados: ");
+            Console.WriteLine(account.AccountData());
+
+
+            Console.Write("\nDigite um valor para saque: ");
+            account.Withdraw(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Conta com dados atualizados: ");
+            Console.WriteLine(account.AccountData());
+        }
+    }
+}
